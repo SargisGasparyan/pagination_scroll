2b88f4b1a0b3851ed0eb20c3525f5e8f8c7727c6
sargis <sargisgasparyan081991@gmail.com>
2021-12-02 16:28:19 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    const [state, setState] = useState([])
+    const [page, setPage] = useState(1)
+    const scrollToEnd = () => {
+        setPage(page + 1)
+    }
+    useEffect(() => {
+        fetch(`https://api.instantwebtools.net/v1/passenger?page=${page}&size=5`).then(res => res.json()).then(json => setState([...state, ...json.data]))
+    }, [page])
+    window.onscroll = function () {
+        if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
+            scrollToEnd()
+        }
+    }
+    return (
+        <div className="App">
+            {state.length > 0 && state.map((el, i) =>
+                <div className={'st'}>
+                    <h4>Id: {el._id}</h4>
+                    <h4>Name: {el.name}</h4>
+                    <h4>Trips: {el.trips}</h4>
+                </div>)}
+        </div>
+    );
 }
 
 export default App;
